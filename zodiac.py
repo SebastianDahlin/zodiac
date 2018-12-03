ed7ddca47052bf6d650589f035b287b353157f44
SebastianDahlin <sebastian.dahlin@gmail.com>
2018-12-03 13:31:35 +0100

    Updated the functions with comments. /SD

--- zodiac.py
+++ zodiac.py
@@ -1,7 +1,9 @@
 def zodiac_input():
+    """Should prompt for user input, validate it and return the value if correct"""
     pass
 
 def get_zodiac():
+    """Should return a zodiac based on a date."""
     date = zodiac_input()
     pass
 
