07036d792c2f7bd4bbf183d8e93427aa635e0886
Sebastian Dahlin <sebastian.dahlin@gmail.com>
2018-12-03 23:48:50 +0100

    Updated the test suite. /SD

--- test_zodiac.py
+++ test_zodiac.py
@@ -1,6 +1,24 @@
 from zodiac import *
+import pytest
 
-def test_zodiac_input():
-    pass
-def test_get_zodiac():
-    pass
+def test_zodiac_input(monkeypatch):
+    with monkeypatch.context() as m:
+        ## First Test
+        m.setattr('builtins.input', lambda x: "1231")
+        date = zodiac_input()
+        assert date == '1231'
+        ## Second test
+        m.setattr('builtins.input', lambda x: "0101")
+        date = zodiac_input()
+        assert date == '0101'
+
+def test_get_zodiac(monkeypatch):
+    with monkeypatch.context() as m:
+        ## First Test
+        m.setattr('builtins.input', lambda x: "0101")
+        zodiac = get_zodiac()
+        assert zodiac == 'Sagittarius'
+        ## Second Test
+        m.setattr('builtins.input', lambda x: "0810")
+        zodiac = get_zodiac()
+        assert zodiac == 'Leo'
