eaad94b598818b9ef1b59aa6b8de40ef746864f5
Jan Lerking <jan.lerking@gmail.com>
2018-12-04 21:14:52 +0100

    Optimized ZODIAC_LIST with INT rather than STR. Optimized get_zodiac function. /JL

--- zodiac.py
+++ zodiac.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
-ZODIAC_LIST = [['Capricornus', '0101', '0119'], ['Aquarius', '0120', '0218'], ['Pisces', '0219', '0320'], ['Aries', '0321', '0419'], ['Taurus', '0420', '0520'], ['Gemini', '0521', '0620'], ['Cancer', '0621', '0722'], ['Leo', '0723', '0822'], ['Virgo', '0823', '0922'], ['Libra', '0923', '1022'], ['Scorpius', '1023', '1121'], ['Sagittarius', '1122', '1221'], ['Capricornus', '1222', '1231']]
+ZODIAC_LIST = [['Capricornus', 101, 119], ['Aquarius', 120, 218], ['Pisces', 219, 320], ['Aries', 321, 419],
+                ['Taurus', 420, 520], ['Gemini', 521, 620], ['Cancer', 621, 722], ['Leo', 723, 822],
+                ['Virgo', 823, 922], ['Libra', 923, 1022], ['Scorpius', 1023, 1121], ['Sagittarius', 1122, 1221], ['Capricornus', 1222, 1231]]
 
 def zodiac_input():
     """Should prompt for user input(no year, just mmdd), validate it and return the value if correct"""
@@ -19,13 +21,9 @@ def zodiac_input():
 
 def get_zodiac(date):
     """Should return a zodiac based on a date."""
-    date = datetime.strptime(date, "%m%d")
-    for zodiac in ZODIAC_LIST:
-        start_date = datetime.strptime(zodiac[1], "%m%d")
-        end_date = datetime.strptime(zodiac[2], "%m%d")
-        if start_date <= date and end_date >= date:
-            return(zodiac[0])
-
+    z = [z for z in ZODIAC_LIST if date in range(z[1], z[2]+1)]
+    return z[0][0]
+    
 if __name__ == "__main__":
-    date = zodiac_input()
+    date = int(zodiac_input())
     print(get_zodiac(date))
